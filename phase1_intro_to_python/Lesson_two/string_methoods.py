cc26ff55c597ddfd07fcc512eaf8ff8b6eedfe46
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-10-14 14:47:37 -0500

    Update string_methoods.py

--- phase1_intro_to_python/Lesson_two/string_methoods.py
+++ phase1_intro_to_python/Lesson_two/string_methoods.py
@@ -23,3 +23,38 @@ print("Does your {} {}?".format(animal, action))
 maria_string = "Maria loves {} and {}"
 print(maria_string.format("math", "statistics"))
 # aria loves math and statistics
+
+# Another important string method: split()
+# This function or method returns a data container called a list that contains the words from the input string. 
+# We will be introducing you to the concept of lists in the next video.
+
+# The split method has two additional arguments (sep and maxsplit). 
+# The sep argument stands for "separator". 
+# It can be used to identify how the string should be split up 
+# (e.g., whitespace characters like space, tab, return, newline; specific punctuation (e.g., comma, dashes)). 
+# If the sep argument is not provided, the default separator is whitespace.
+
+# True to its name, the maxsplit argument provides the maximum number of splits.
+# The argument gives maxsplit + 1 number of elements in the new list, with the remaining string being returned as the last element in the list. 
+# You can read more about these methods in the Python documentation too.
+
+# A basic split method:
+
+new_str = "The cow jumped over the moon."
+new_str.split()
+# Output is: ['The', 'cow', 'jumped', 'over', 'the', 'moon.']
+
+
+# Here the separator is space, and the maxsplit argument is set to 3.
+new_str.split(' ', 3)
+# Output is: ['The', 'cow', 'jumped', 'over the moon.']
+
+
+# Using '.' or period as a separator.
+new_str.split('.')
+# Output is: ['The cow jumped over the moon', '']
+
+
+# Using no separators but having a maxsplit argument of 3.
+new_str.split(None, 3)
+# Output is: ['The', 'cow', 'jumped', 'over the moon.']
