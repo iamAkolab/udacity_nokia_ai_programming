6ca2113cc3d0e67d5fd7656d13e2e318dfd07d53
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-10-27 01:43:38 -0500

    Update for_practice.py

--- phase1_intro_to_python/Lesson_four/for_practice.py
+++ phase1_intro_to_python/Lesson_four/for_practice.py
@@ -43,5 +43,61 @@ This should output:
 for i in range(5, 35, 5):
     print(i)
 
+---------------------------------------------------------------------------------------------------------------------
+"""
+Quiz: Create Usernames
+Write a for loop that iterates over the names list to create a usernames list. To create a username for each name, 
+make everything lowercase and replace spaces with underscores. Running your for loop over the list:
 
-  
+names = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+
+should create the list:
+
+usernames = ["joey_tribbiani", "monica_geller", "chandler_bing", "phoebe_buffay"]
+
+HINT: Use the .replace() method to replace the spaces with underscores. Check out how to use this method in this
+"""
+names = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+usernames = []
+
+# write your for loop here
+for name in names:
+    usernames.append(name.lower().replace(" ", "_"))
+
+
+print(usernames)
+---------------------------------------------------------------------------------------------------------------------
+"""
+Modify Usernames with Range
+Write a for loop that uses range() to iterate over the positions in usernames to modify the list. Like you did in the previous quiz, change each name to be lowercase and replace spaces with underscores. After running your loop, this list
+
+usernames = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+
+should change to this:
+
+usernames = ["joey_tribbiani", "monica_geller", "chandler_bing", "phoebe_buffay"]
+"""
+
+usernames = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+
+# write your for loop here
+for i in range(len(usernames)):
+    usernames[i] = usernames[i].lower().replace(" ", "_")
+
+print(usernames)
+---------------------------------------------------------------------------------------------------------------------
+"""
+Tag Counter
+Write a for loop that iterates over a list of strings, tokens, and counts how many of them are XML tags. XML is a data language similar to HTML. You can tell if a string is an XML tag if it begins with a left angle bracket "<" and ends with a right angle bracket ">". Keep track of the number of tags using the variable count.
+
+You can assume that the list of strings will not contain empty strings.
+"""
+tokens = ['<greeting>', 'Hello World!', '</greeting>']
+count = 0
+
+# write your for loop here
+for token in tokens:
+    if token[0] == '<' and token[-1] == '>':
+        count += 1
+
+print(count)
