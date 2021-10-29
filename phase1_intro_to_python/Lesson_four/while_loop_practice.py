b5cff6fd77c29e336c16f4a93dbf9ee85117e830
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-10-28 23:27:20 -0500

    Update while_loop_practice.py

--- phase1_intro_to_python/Lesson_four/while_loop_practice.py
+++ phase1_intro_to_python/Lesson_four/while_loop_practice.py
@@ -1,4 +1,27 @@
-*""" 
+#------------------------------------------------------------------------------------------------------------
+"""
+For Loops Vs. While Loops
+Now that you are familiar with both for and while loops, let's consider when it's most helpful to use each of them.
+
+for loops are ideal when the number of iterations is known or finite.
+
+Examples:
+
+When you have an iterable collection (list, string, set, tuple, dictionary)
+for name in names:
+When you want to iterate through a loop for a definite number of times, using range()
+for i in range(5):
+while loops are ideal when the iterations need to continue until a condition is met.
+
+Examples:
+
+When you want to use comparison operators
+while count <= 100:
+When you want to loop based on receiving specific user input.
+while user_input == 'y':
+"""
+#------------------------------------------------------------------------------------------------------------
+""" 
 Factorials with While Loops
 Find the factorial of a number using a while loop.
 
@@ -126,3 +149,31 @@ nearest_square = num**2
 
 print(nearest_square)
 #------------------------------------------------------------------------------------------------------------
+"""
+What type of loop should we use?
+You need to write a loop that takes the numbers in a given list named num_list:
+num_list = [422, 136, 524, 85, 96, 719, 85, 92, 10, 17, 312, 542, 87, 23, 86, 191, 116, 35, 173, 45, 149, 59, 84, 69, 113, 166]
+
+Your code should add up the odd numbers in the list, but only up to the first 5 odd numbers together. 
+If there are more than 5 odd numbers, you should stop at the fifth. If there are fewer than 5 odd numbers, add all of the odd numbers.
+
+Would you use a while or a for loop to write this code?
+
+We have provided our solution on the next page. Feel free to use the coding playground below to test your code.
+"""
+## Please use this space to test and run your code
+num_list = [422, 136, 524, 85, 96, 719, 85, 92, 10, 17, 312, 542, 87, 23, 86, 191, 116, 35, 173, 45, 149, 59, 84, 69, 113, 166]
+
+count_odd = 0
+list_sum = 0
+i = 0
+len_num_list = len(num_list)
+
+while (count_odd < 5) and (i < len_num_list): 
+    if num_list[i] % 2 != 0:
+        list_sum += num_list[i]
+        count_odd += 1
+    i += 1
+
+print ("The numbers of odd numbers added are: {}".format(count_odd))
+print ("The sum of the odd numbers added is: {}".format(list_sum))
