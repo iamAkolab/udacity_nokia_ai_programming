3cd2ef441f2901ad0fc27439925968c73dd38dda
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-11-09 02:15:00 -0600

    Create additional_resources.py

---
+++ phase1_intro_to_python/Lesson_five/additional_resources.py
@@ -0,0 +1,6 @@
+# Additional Resources
+# If you want to learn more about writing functions, check out this talk from PyCon by Jack Diederich (https://youtu.be/rrBJVMyD-Gs). 
+# Diederich covers best practices for writing functions in Python that also apply to all code in Python.
+
+# Here's a great blog post about yield  and generators from Jeff Knupp.
+# (https://jeffknupp.com/blog/2013/04/07/improve-your-python-yield-and-generators-explained/)
