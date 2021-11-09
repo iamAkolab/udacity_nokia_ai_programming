d817cd5d5b051b01670cfe0a11e1353178026c6e
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-11-09 02:09:34 -0600

    Update iterator_generator.py

--- phase1_intro_to_python/Lesson_five/iterator_generator.py
+++ phase1_intro_to_python/Lesson_five/iterator_generator.py
@@ -72,3 +72,34 @@ for i, lesson in my_enumerate(lessons, 1):
 
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------
+# Quiz: Chunker
+# If you have an iterable that is too large to fit in memory in full (e.g., when dealing with large files), being able to take and use chunks of it at a time can be very valuable.
+
+# Implement a generator function, chunker, that takes in an iterable and yields a chunk of a specified size at a time.
+
+# Calling the function like this:
+
+for chunk in chunker(range(25), 4):
+    print(list(chunk))
+
+"""
+should output:
+
+[0, 1, 2, 3]
+[4, 5, 6, 7]
+[8, 9, 10, 11]
+[12, 13, 14, 15]
+[16, 17, 18, 19]
+[20, 21, 22, 23]
+[24]
+
+"""
+def chunker(iterable, size):
+    # Implement function here
+    """Yield successive chunks from iterable of length size."""
+    for i in range(0, len(iterable), size):
+        yield iterable[i:i + size]
+
+
+for chunk in chunker(range(25), 4):
+    print(list(chunk))
