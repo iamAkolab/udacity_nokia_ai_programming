55d350ecaff60b07ece386a864dc75b06636fd3c
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-11-11 23:14:24 -0600

    Create import_localscript_practice.py

---
+++ phase1_intro_to_python/Lesson_six/import_localscript_practice.py
@@ -0,0 +1,96 @@
+# Quiz: Compute an Exponent
+# It's your turn to import and use the math module. Use the math module to calculate e to the power of 3. print the answer.
+
+# Refer to the math module's documentation to find the function you need!
+
+# print e to the power of 3 using the math module
+import math
+
+print(math.exp(3))
+
+#--------------------------------------------------------------------------------------------------------------------------------
+# Quiz: Password Generator
+# Write a function called generate_password that selects three random words from the list of words word_list and concatenates them into a single string. 
+# Your function should not accept any arguments and should reference the global variable word_list to build the password.
+
+# password_generator.py
+# TODO: First import the `random` module
+import random
+
+# We begin with an empty `word_list`
+word_file = "words.txt"
+word_list = []
+
+# We fill up the word_list from the `words.txt` file
+with open(word_file,'r') as words:
+	for line in words:
+		# remove white space and make everything lowercase
+		word = line.strip().lower()
+		# don't include words that are too long or too short
+		if 3 < len(word) < 8:
+			word_list.append(word)
+
+# TODO: Add your function generate_password below
+# It should return a string consisting of three random words 
+# concatenated together without spaces
+def generate_password():
+    return random.choice(word_list) + random.choice(word_list) + random.choice(word_list)
+
+
+
+# Now we test the function
+print(generate_password())
+
+# words.txt
+Alice
+was
+beginning
+to
+get
+very
+tired
+of
+sitting
+by
+her
+sister
+bank
+having
+nothing
+Once
+twice
+she
+had
+peeped
+into
+the
+book
+her
+sister
+was
+reading
+but
+it
+had
+no
+pictures
+or
+conversations
+in
+it
+and
+what
+is
+the
+use
+of
+a
+book
+thought
+Alice
+without
+pictures
+or
+conversations
+
+#--------------------------------------------------------------------------------------------------------------------------------
