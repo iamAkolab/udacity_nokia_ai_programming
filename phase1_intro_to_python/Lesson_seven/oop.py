77393d4ff6f504ce96c9c78e8ab80f05bc464563
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-11-14 22:52:58 -0600

    Update oop.py

--- phase1_intro_to_python/Lesson_seven/oop.py
+++ phase1_intro_to_python/Lesson_seven/oop.py
@@ -1,1 +1,31 @@
-#
+# Why Object-Oriented Programming
+"""
+Object-oriented programming has a few benefits over procedural programming, which is the programming style you first learned.
+
+Object-oriented programming allows you to create large, modular programs that can easily expand over time.
+Object-oriented programs hide implementation from the end-userConsider Python package like scikit-learn, pandas, or numpy. 
+These are all Python libraries built with object-oriented programming. 
+Scikit-learn, for example, is a relatively large and complex package built with object-oriented programming. 
+This package has expanded over the years with new functionality and new algorithms
+
+Consider Python package like scikit-learn, pandas, or numpy. These are all Python libraries built with object-oriented programming. 
+Scikit-learn, for example, is a relatively large and complex package built with object-oriented programming. 
+This package has expanded over the years with new functionality and new algorithms
+"""
+
+##Object-oriented programming (OOP) vocabulary
+"""
+Class: A blueprint consisting of methods and attributes.
+
+Object: An instance of a class. It can help to think of objects as something in the real world like a yellow pencil, a small dog, or a blue shirt.
+However, as you'll see later in the lesson, objects can be more abstract.
+
+Attribute: A descriptor or characteristic. Examples would be color, length, size, etc. These attributes can take on specific values like blue, 3 inches, large, etc.
+
+Method: An action that a class or object could take.
+
+OOP: A commonly used abbreviation for object-oriented programming.
+
+Encapsulation: One of the fundamental ideas behind object-oriented programming is called encapsulation: you can combine functions and data all into a single entity. In object-oriented programming, this single entity is called a class. 
+Encapsulation allows you to hide implementation details, much like how the scikit-learn package hides the implementation of machine learning algorithms.
+"""
