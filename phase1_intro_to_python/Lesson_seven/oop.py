0a190d906385021e3d74c591b797152e7a27a5aa
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-11-15 13:58:29 -0600

    Update oop.py

--- phase1_intro_to_python/Lesson_seven/oop.py
+++ phase1_intro_to_python/Lesson_seven/oop.py
@@ -29,3 +29,43 @@ OOP: A commonly used abbreviation for object-oriented programming.
 Encapsulation: One of the fundamental ideas behind object-oriented programming is called encapsulation: you can combine functions and data all into a single entity. In object-oriented programming, this single entity is called a class. 
 Encapsulation allows you to hide implementation details, much like how the scikit-learn package hides the implementation of machine learning algorithms.
 """
+
+# Object-oriented programming syntax
+""""
+Function versus method
+A function and a method look very similar. They both use the def keyword. They also have inputs and return outputs. 
+The difference is that a method is inside of a class whereas a function is outside of a class.
+Finally, you'll write your own class.
+"""
+class Shirt:
+  def __init__(self, shirt_color, short_size, shirt_style, shirt_price):
+    self.color = shirt_color
+    self.size = shirt_size
+    self.style = shirt_style
+    self.price = shirt_price
+    
+  def change_price(self, new_price):
+    self.price = new_price
+    
+  def discount(self, discount):
+    return self.price * (1 - discount)
+  
+"""
+What is self?
+If you instantiate two objects, how does Python differentiate between these two objects?
+
+shirt_one = Shirt('red', 'S', 'short-sleeve', 15)
+shirt_two = Shirt('yellow', 'M', 'long-sleeve', 20)
+
+That's where self comes into play. If you call the change_price method on shirt_one, how does Python know to change the price of shirt_one and not of shirt_two?
+
+shirt_one.change_price(12)
+Behind the scenes, Python is calling the change_price method:
+
+    def change_price(self, new_price):
+
+        self.price = new_price
+Self tells Python where to look in the computer's memory for the shirt_one object. Then, Python changes the price of the shirt_one object. When you call the change_price method, shirt_one.change_price(12), self is implicitly passed in.
+
+The word self is just a convention. You could actually use any other name as long as you are consisten, but you should use self to avoid confusing people.
+"""
