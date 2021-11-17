e5a1c044694f64017ef91293f0a8141644db7357
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-11-16 22:53:43 -0600

    Create advanced_oop_topics.py

---
+++ phase1_intro_to_python/Lesson_seven/advanced_oop_topics.py
@@ -0,0 +1,20 @@
+"""
+Advanced OOP topics
+Inheritance is the last object-oriented programming topic in the lesson. Thus far you've been exposed to:
+
+Classes and objects
+Attributes and methods
+Magic methods
+Inheritance
+Classes, object, attributes, methods, and inheritance are common to all object-oriented programming languages.
+
+Knowing these topics is enough to start writing object-oriented software. What you've learned so far is all you need to know to complete this OOP lesson. However, these are only the fundamentals of object-oriented programming.
+
+Use the following list of resources to learn more about advanced Python object-oriented programming topics.
+
+Python's Instance, Class, and Static Methods Demystified: This article explains different types of methods that can be accessed at the class or object level.
+Class and Instance Attributes: You can also define attributes at the class level or at the instance level.
+Mixins for Fun and Profit: A class can inherit from multiple parent classes.
+Primer on Python Decorators: Decorators are a short-hand way to use functions inside other functions.
+
+"""
