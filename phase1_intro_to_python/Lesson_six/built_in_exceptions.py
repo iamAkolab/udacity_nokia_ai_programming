71603cd877a3289c9a9f692cfa6467c6c447744a
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-03-04 22:20:03 -0600

    Create built_in_exceptions.py

---
+++ phase1_intro_to_python/Lesson_six/built_in_exceptions.py
@@ -0,0 +1,11 @@
+#------------------------------------------------------------------------------------------------------------------------------#
+# ValueError -     An object of the correct type but inappropriate value is passed as input to a built-in operation or function
+
+# AssertionError - An assert statement fails
+
+# IndexError -    A sequence subscripts id out of range
+
+# KeyError -     A key can't be found in a dictionary
+
+# TypeError -    An object of an unsupported type is passed as input to an operation or function.
+#------------------------------------------------------------------------------------------------------------------------------#
