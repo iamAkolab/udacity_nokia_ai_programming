1bf8957b8b89b2c3bcd0c22163caa0c81a0c19fd
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-03-08 23:13:25 -0600

    Create contributing_to_githubproject.py

---
+++ phase2_AI_Programming_nanodegree/core_curriculum/intro_to_python/lesson_seven/contributing_to_githubproject.py
@@ -0,0 +1,14 @@
+## Contributing to a GitHub Project
+
+# Use the following resources to learn how to contribute to a GitHub project:
+
+# Beginner's Guide to Contributing to a Github Project  (https://akrabat.com/the-beginners-guide-to-contributing-to-a-github-project/)
+# Contributing to a GitHub Project (https://github.com/MarcDiethelm/contributing/blob/master/README.md)
+
+
+
+## Advanced Python OOP Topics
+# Use the following resources to learn about more advanced OOP topics that appear in the scikit-learn package:
+
+# Decorators  (https://realpython.com/primer-on-python-decorators/)
+# Mixins (https://easyaspython.com/mixins-for-fun-and-profit-cb9962760556)
