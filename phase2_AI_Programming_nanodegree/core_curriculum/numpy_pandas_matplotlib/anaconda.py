a55c28b2a1719284c3eb2d8cdfac479af45d8cc2
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-04-03 22:41:54 -0500

    Create anaconda.py

---
+++ phase2_AI_Programming_nanodegree/core_curriculum/numpy_pandas_matplotlib/anaconda.py
@@ -0,0 +1,14 @@
+# The list of commands used are in the video above are:
+
+'''
+conda create -n tea_facts python=3
+source activate tea_facts
+conda list
+conda install numpy pandas matplotlib 
+'''
+
+# Anaconda comes with the Jupyter notebook package. If you are using Miniconda, you can install Jupyter notebook as:
+
+'''
+conda install jupyter notebook
+'''
